91bc952d8f3c2f75b388a2c9fae7a3b954ea36e8
victorclzz <160596951+victorclzz@users.noreply.github.com>
2024-10-22 07:48:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos planetas existem no Sistema Solar??',
+    'existem 8 planetas'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'What is the capital of the United Kingdom??',
+    'London'
+)
